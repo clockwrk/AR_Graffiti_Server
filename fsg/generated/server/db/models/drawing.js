3fedd4370b7512a37fd6d799dd41c089fea50ad0
Jose <Jrenteria2016@outlook.com>
2016-10-01 13:11:12 -0400

    working start

--- fsg/generated/server/db/models/drawing.js
+++ fsg/generated/server/db/models/drawing.js
@@ -1,15 +1,15 @@
-'use strict';
-
-var _ = require('lodash');
-var Sequelize = require('sequelize');
-
-var db = require('../_db');
-
-module.exports = db.define('drawing', {
-  strokes: {
-    type: Sequelize.ARRAY(Sequelize.ARRAY)
-  },
-  angle: {
-    type: Sequelize.INTEGER
-  }
-});
+// 'use strict';
+//
+// var _ = require('lodash');
+// var Sequelize = require('sequelize');
+//
+// var db = require('../_db');
+//
+// module.exports = db.define('drawing', {
+//   strokes: {
+//     type: Sequelize.ARRAY(Sequelize.ARRAY)
+//   },
+//   angle: {
+//     type: Sequelize.INTEGER
+//   }
+// });
